fcd3f643232ee55a01af0a04a91c02954b6f23ff
mixos <mixos@localhost.localdomain>
2015-09-05 17:30:58 -0700

    using meta data sturct in fbfs.c

--- fbfs.c
+++ fbfs.c
@@ -10,8 +10,8 @@
 int main()
 {
 
-    FS_META fm;
-    FS_META fm_read;
+    FS_META *fm;
+    FS_META *fm_read;
     int fd = -1;
     int i = 0;
     char *c = NULL;
@@ -30,15 +30,14 @@ int main()
         printf("error in truncate");
        exit(0);
     }
-/*
-    fm = (FS_META *)malloc(sizeof(FS_META));
-*/
 
-    strcpy(fm.name, "foo_fs");
-    fm.meta_type= 0; /* meta data type: 0 - root dir */
-    strcpy(fm.rootdir, "/");
-    fm.fs_size = 512 * 1024;
-    fm.block_size = 512;
+    fm = (FS_META *)malloc(sizeof(FS_META));
+
+    strcpy(fm->name, "foo_fs");
+    fm->meta_type= 0; /* meta data type: 0 - root dir */
+    strcpy(fm->rootdir, "/");
+    fm->fs_size = 512 * 1024;
+    fm->block_size = 512;
     
     
     
@@ -48,13 +47,13 @@ int main()
         exit(0);
     }
 
-    fprintf(stdout, "Writing ....");
-    memcpy(addr, &fm, sizeof(FS_META));
+    fprintf(stdout, "Writing to %s....\n", FN);
+    memcpy(addr, fm, sizeof(FS_META));
 
     close(fd);
 
 
-    fprintf(stdout, "OPENING ....");
+    fprintf(stdout, "OPENING %s to read data ....\n", FN);
     fd = open(FN, O_RDWR| O_EXCL, S_IRUSR | S_IWUSR);
 
     if (fd < 0) {
@@ -67,8 +66,14 @@ int main()
         printf("Error in mmap");
         exit(0);
     }
-    memcpy(&fm_read, addr, sizeof(FS_META));
 
-    fprintf(stdout, fm_read.name);
+    fm_read = (FS_META *)malloc(sizeof(FS_META));
+    memcpy(fm_read, addr, sizeof(FS_META));
+
+    fprintf(stdout, "name: %s\n", fm_read->name);
+    fprintf(stdout, "meta_type: %d\n", fm_read->meta_type);
+    fprintf(stdout, "rootdir: %s\n", fm_read->rootdir);
+    fprintf(stdout, "fs_size: %d\n", fm_read->fs_size);
+    fprintf(stdout, "block_size: %d\n", fm_read->block_size);
 
 } 
